43f454495758e6faac52022560ea339b7c2e28b0
M1ker1n <m1ker1nigor@yandex.ru>
2022-04-25 13:46:49 +0300

    CWA-30-last-added-items

--- CollectionWebApp/CollectionWebApp/Controllers/HomeController.cs
+++ CollectionWebApp/CollectionWebApp/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using CollectionWebApp.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+
+using CollectionWebApp.Models;
+using CollectionWebApp.ViewModels;
 
 namespace CollectionWebApp.Controllers
 {
@@ -8,14 +10,22 @@ namespace CollectionWebApp.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private AppDbContext db;
+
+        public HomeController(ILogger<HomeController> logger, AppDbContext db)
         {
             _logger = logger;
+            this.db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var items = (db.Items.Count() > 10) ? db.Items.OrderByDescending(i => i.Created).SkipLast(db.Items.Count() - 10) : db.Items;
+            HomeIndexModel model = new HomeIndexModel()
+            {
+                Items = items.ToList()
+            };
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ CollectionWebApp/CollectionWebApp/ViewModels/HomeIndexModel.cs
@@ -0,0 +1,9 @@
+﻿using CollectionWebApp.Models;
+
+namespace CollectionWebApp.ViewModels
+{
+    public class HomeIndexModel
+    {
+        public ICollection<Item> Items { get; set; } = null!;
+    }
+}
