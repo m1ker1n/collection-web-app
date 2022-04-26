213c48a35552a87656bb9b8b5876e914756cb972
M1ker1n <m1ker1nigor@yandex.ru>
2022-04-26 19:12:36 +0300

    CWA-37-register-methods

--- CollectionWebApp/CollectionWebApp/Controllers/AuthorizationController.cs
+++ CollectionWebApp/CollectionWebApp/Controllers/AuthorizationController.cs
@@ -58,5 +58,47 @@ namespace CollectionWebApp.Controllers
             var id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
             await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
         }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (User.Identity.IsAuthenticated) return RedirectToAction("Index", "Account");
+            ValidateRegisterModel(model);
+            if (!ModelState.IsValid) return View(model);
+            User? appUser = await RegisterUserAsync(model);
+            await LoginUserAsync(appUser!);
+            return RedirectToAction("Index", "Account");
+        }
+
+        private void ValidateRegisterModel(RegisterModel model)
+        {
+            User? user = db.Users.FirstOrDefault(u => u.Email == model.Email);
+            if (user != null) ModelState.AddModelError("", "This email's already registered.");
+            user = db.Users.FirstOrDefault(u => u.Name == model.Name);
+            if (user != null) ModelState.AddModelError("", "This nickname's already registered.");
+        }
+
+        private async Task<User?> RegisterUserAsync(RegisterModel model)
+        {
+            Role? userRole = await db.Roles.FindAsync(db.UserRoleId);
+            if (userRole == null) return null;
+            User? user = new User
+            {
+                Email = model.Email,
+                Name = model.Name,
+                Password = model.Password,
+                Role = userRole
+            };
+            await db.Users.AddAsync(user);
+            await db.SaveChangesAsync();
+            return user;
+        }
     }
 }
