2aa602a7929d516d835e256533bd78d49a1bf940
M1ker1n <m1ker1nigor@yandex.ru>
2022-04-26 18:57:04 +0300

    CWA-33-register-view

---
+++ CollectionWebApp/CollectionWebApp/ViewModels/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollectionWebApp.ViewModels
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
