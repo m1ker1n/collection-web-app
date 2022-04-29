37e784ca81a766a1b5b8b524fa9f392f520f6043
M1ker1n <m1ker1nigor@yandex.ru>
2022-04-29 22:13:11 +0300

    CWA-40-block-to-see-user-data

---
+++ CollectionWebApp/CollectionWebApp/ViewModels/AccountModel.cs
@@ -0,0 +1,11 @@
+﻿using CollectionWebApp.Models;
+
+namespace CollectionWebApp.ViewModels
+{
+    public class AccountModel
+    {
+        public User User { get; set; } = null!;
+
+        public bool ChangeAllowed { get; set; }
+    }
+}
