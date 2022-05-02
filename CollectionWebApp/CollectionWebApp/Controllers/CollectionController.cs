72d0c7444d20228bfc8fb2f569c884f2e07898b9
M1ker1n <m1ker1nigor@yandex.ru>
2022-05-02 14:02:33 +0300

    CWA-57-collection-page

---
+++ CollectionWebApp/CollectionWebApp/Controllers/CollectionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using CollectionWebApp.Models;
+using CollectionWebApp.ViewModels;
+using CollectionWebApp.Extensions;
+
+namespace CollectionWebApp.Controllers
+{
+    public class CollectionController : Controller
+    {
+        private AppDbContext db;
+
+        public CollectionController(AppDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            UserCollection? collection = await db.UserCollections.FindAsync(id);
+            var model = CreateIndexModel(collection, User.HasAccessTo(collection?.User, db));
+            return model == null ? NotFound() : View(model);
+        }
+
+        private CollectionIndexModel? CreateIndexModel(UserCollection? collection, bool changeAllowed = false)
+        {
+            if (collection == null) return null;
+            CollectionIndexModel model = new()
+            {
+                Collection = collection,
+                ChangeAllowed = changeAllowed
+            };
+            return model;
+        }
+    }
+}
